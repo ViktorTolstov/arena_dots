3dc2a976fedb8882ec7b7c2f3df62418a12093f6
Viktor Tolstov <viktor.tolstov.tsu@gmail.com>
2023-10-19 00:06:01 +0600

    fixes

--- Assets/Scripts/Gameplay/GameplaySceneContext.cs
+++ Assets/Scripts/Gameplay/GameplaySceneContext.cs
@@ -35,10 +35,10 @@ namespace ForiDots
             _gameController = new GameController();
             _gameController.Setup(_gameModel);
 
-            AGInGameUIController inGameUIController = ArenaGamesController.Instance.GetInGameUIController();
-            UIElement_StartGamePanel startPanel = (UIElement_StartGamePanel)inGameUIController.GetUIElement<UIElement_StartGamePanel>();
-            UIElement_GameOverPanel gameOverPanel = (UIElement_GameOverPanel)inGameUIController.GetUIElement<UIElement_GameOverPanel>();
-            UIElement_GameHUD gameHUD = (UIElement_GameHUD)inGameUIController.GetUIElement<UIElement_GameHUD>();
+            var inGameUIController = ArenaGamesController.Instance.GetInGameUIController();
+            var startPanel = (UIElement_StartGamePanel)inGameUIController.GetUIElement<UIElement_StartGamePanel>();
+            var gameOverPanel = (UIElement_GameOverPanel)inGameUIController.GetUIElement<UIElement_GameOverPanel>();
+            var gameHUD = (UIElement_GameHUD)inGameUIController.GetUIElement<UIElement_GameHUD>();
             startPanel.gameStarted += () => { _gameView.Setup(_gameController, gameOverPanel, gameHUD); };
         }
     }
